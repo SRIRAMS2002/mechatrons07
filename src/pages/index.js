3cb56f738c50fe719c9e01271a7c062d22af2ae9
SRIRAMS2002 <sriramsrit.2002@gmail.com>
2023-09-14 22:29:15 +0530

    Navbar Finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-"use-client"
-
 import React from 'react'
-
+import Navbar from '@/Components/Navbar'
 const index = () => {
   return (
     <div>
-      sriram
+      
+      <Navbar/>
+      
     </div>
   )
 }
