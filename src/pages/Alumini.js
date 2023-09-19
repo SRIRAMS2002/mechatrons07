e12083e8c2a8eff5a108b7e10cba7824a93da32e
SRIRAMS2002 <sriramsrit.2002@gmail.com>
2023-09-19 22:36:20 +0530

    routed

---
+++ src/pages/Alumini.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Alumini = () => {
+  return (
+    <div>
+       Alumini
+
+    </div>
+  )
+}
+
+export default Alumini
---
+++ src/pages/Mentors.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Mentors = () => {
+  return (
+    <div>
+      hixss
+    </div>
+  )
+}
+
+export default Mentors
