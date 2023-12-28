da4d73a6b39f5717494b03d6a1e3e0f3b50729c9
Aakash T <aakashvnth@gmail.com>
2023-12-28 11:05:35 +0530

    changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 import React from 'react'
-import Navbar from '@/Components/Navbar'
-import Footer from '@/Components/Footer';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Autoplay, Pagination, Scrollbar, A11y } from "swiper/modules";
 // Import Swiper styles
 import 'swiper/css';
-import {motion} from "framer-motion"
-=======
-import React from "react";
 import Navbar from "@/Components/Navbar";
 import Hero from "@/Components/Hero";
 import About from "@/Components/About";
@@ -17,9 +11,9 @@ import News from "@/Components/News";
 import Projects from "@/Components/Projects";
 import Testimonials from "@/Components/Testimonials";
 import Footer from "@/Components/Footer";
-import Headerimg from "../../public/2.jpg";
+// import Headerimg from "../../public/2.jpg";
 import { motion } from "framer-motion";
->>>>>>> ac73d0c (hero and motion added {not good})
+
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 const index = () => {
